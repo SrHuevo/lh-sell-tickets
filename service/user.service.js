75b80b04ce0fb06113190ad581a86ec49d35ab29
SrHuevo <dgaroz@lan.krama.es>
2016-09-15 03:01:51 +0200

    log

--- service/user.service.js
+++ service/user.service.js
@@ -2,9 +2,12 @@ var User = require('../model/user.model');
 var CryptoUtil = require('../util/crypto.util');
 
 module.exports.isPassCorrect = function(user, success, error){
-    console.log(user);
+    console.log(user.name);
     console.log(process.env.NAME_MASTER_LHT);
+    console.log(user.name===process.env.NAME_MASTER_LHT);
+    console.log(user.pass);
     console.log(process.env.PASS_MASTER_LHT);
+    console.log(user.pass===process.env.PASS_MASTER_LHT);
     if(user.name===process.env.NAME_MASTER_LHT && user.pass === process.env.PASS_MASTER_LHT){
         console.log("Autenticado usuario master");
         success();
