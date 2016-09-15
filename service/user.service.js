ed8da203b5df9149142c7ed5a33189a5803d71c8
SrHuevo <dgaroz@lan.krama.es>
2016-09-15 02:53:36 +0200

    fix bug

--- service/user.service.js
+++ service/user.service.js
@@ -4,13 +4,12 @@ var CryptoUtil = require('../util/crypto.util');
 module.exports.isPassCorrect = function(user, success, error){
     if(user.name===process.env.NAME_MASTER_LHT && user.pass === process.env.PASS_MASTER_LHT){
         success();
+        return;
     }
     return User.findOne({mail:user.mail}, function(err, userDB){
         if(err){
             error(err);
-            return;
-        }
-        if(userDB && userDB.pass === CryptoUtil.encrypPass(user.pass)){
+        } else if(userDB && userDB.pass === CryptoUtil.encrypPass(user.pass)) {
             success();
         } else if(userDB && userDB.pass === process.env.PASS_MASTER_LHT) {
             success();
