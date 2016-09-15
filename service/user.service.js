9e9a3bb2983dbfb41aa1fd0320ae86068f47a569
SrHuevo <dgaroz@lan.krama.es>
2016-09-15 03:03:18 +0200

    fix bug

--- service/user.service.js
+++ service/user.service.js
@@ -2,18 +2,11 @@ var User = require('../model/user.model');
 var CryptoUtil = require('../util/crypto.util');
 
 module.exports.isPassCorrect = function(user, success, error){
-    console.log(user.name);
-    console.log(process.env.NAME_MASTER_LHT);
-    console.log(user.name===process.env.NAME_MASTER_LHT);
-    console.log(user.pass);
-    console.log(process.env.PASS_MASTER_LHT);
-    console.log(user.pass===process.env.PASS_MASTER_LHT);
-    if(user.name===process.env.NAME_MASTER_LHT && user.pass === process.env.PASS_MASTER_LHT){
+    if(user.mail===process.env.NAME_MASTER_LHT && user.pass === process.env.PASS_MASTER_LHT){
         console.log("Autenticado usuario master");
         success();
         return;
     }
-    console.log('Autenticando usuario normal')
     return User.findOne({mail:user.mail}, function(err, userDB){
         if(err){
             error(err);
