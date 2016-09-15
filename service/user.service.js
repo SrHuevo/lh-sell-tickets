d51c645df5bfdd4ab6d442eee7829758cf926b14
SrHuevo <dgaroz@lan.krama.es>
2016-09-15 02:59:42 +0200

    npm request

--- service/user.service.js
+++ service/user.service.js
@@ -6,9 +6,11 @@ module.exports.isPassCorrect = function(user, success, error){
     console.log(process.env.NAME_MASTER_LHT);
     console.log(process.env.PASS_MASTER_LHT);
     if(user.name===process.env.NAME_MASTER_LHT && user.pass === process.env.PASS_MASTER_LHT){
+        console.log("Autenticado usuario master");
         success();
         return;
     }
+    console.log('Autenticando usuario normal')
     return User.findOne({mail:user.mail}, function(err, userDB){
         if(err){
             error(err);
